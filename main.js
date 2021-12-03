dfe34cbb3bd3b8d06c8c71911ed75a7a3fd7337a
khaled39ab <khaled39ab@gmail.com>
2021-12-03 11:23:32 +0600

    verifing pin

--- main.js
+++ main.js
@@ -47,4 +47,37 @@ document.getElementById ('remove-last-digit').addEventListener ('click', functio
     document.getElementById ('typed-pin').value = removeLastDigit;
 });
 
+ //verify pin
+function verifyPin(){
+    const pin = document.getElementById('pin').value;
+    const typedPin= document.getElementById ('typed-pin').value;
+    if (pin === '' || typedPin === ''){
+        return;
+    }
+    else{
+        if (pin === typedPin){
+            const matched = document.getElementById ('matched');
+            matched.style.display = 'block';
+            document.getElementById ('font-page').style.display = 'none';
+        }
+        else{
+            const notMatched = document.getElementById ('notMatched');
+            notMatched.style.display = 'block';
+            document.getElementById ('font-page').style.display = 'none';
+        }
+    }
+};
+document.getElementById ('submitBtn').addEventListener ('click', function (){
+    verifyPin();
+});
 
+//Retry event
+document.getElementById ('retry-btn').addEventListener ('click', function(){
+    const secondTry = document.getElementById ('font-page');
+    secondTry.style.display = 'block';
+    document.getElementById ('verify').style.display = 'none';
+    const pin = document.getElementById('pin');
+    pin.value = '';
+    const typedPin= document.getElementById ('typed-pin');
+    typedPin.value = '';
+});
