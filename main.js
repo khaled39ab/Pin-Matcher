32c5134dc647f3f1d1cabaa03205bc2f595ca359
khaled39ab <khaled39ab@gmail.com>
2021-12-25 22:01:43 +0600

    3 chance feature added

--- main.js
+++ main.js
@@ -1,99 +1,111 @@
+let submitLeft = 3;
+
+document.getElementById("pin").disabled = true; //textBox disable
+
 function getPin() {
-    const randomNumber = Math.random() * 100000;
+    const randomNumber = Math.floor(Math.random() * 90000) + 10000;
     console.log(randomNumber);
-    const pin = (randomNumber + '').split('.')[0];
-    if (pin.length === 5) {
-        return pin;
-    }
-    else {
-        console.log('Not valid ' + pin);
-        return getPin();
-    }
-    // const randomNum = Math.ceil (randomNumber);
-    // console.log(randomNum);
-    // return randomNum;
+    // const randomNumber = Math.random() * 100000;
+    //const pin = (randomNumber + '').split('.')[0];
+    // if (pin.length === 5) {
+    //     return pin;
+    // }
+    // else {
+    //     console.log('Not valid ' + pin);
+    //     return getPin();
+    // }
+    return randomNumber;
 };
 
 //display generated pin
 function generatePin() {
     const pinInput = document.getElementById('pin');
     pinInput.value = getPin();
+    pinInput.style.color = "black";
 };
-document.getElementById ('generateBtn').addEventListener ('click', function(){
+document.getElementById('generateBtn').addEventListener('click', function () {
     generatePin();
 });
 
 //handle calculator buttons
-const buttonsContainer = document.getElementById ('buttons-container');
-buttonsContainer.addEventListener ('click', function (event){
+const buttonsContainer = document.getElementById('buttons-container');
+buttonsContainer.addEventListener('click', function (event) {
     const digits = event.target.innerText;
-    if (isNaN(digits)){
+    if (isNaN(digits)) {
 
     }
-    else{
-        const typedInput = document.getElementById ('typed-pin');
+    else {
+        const typedInput = document.getElementById('typed-pin');
         //typedInput.value = typedInput.value + digits;
         typedInput.value += digits;
     }
 });
 
 //clear input data
-document.getElementById ('clear-btn').addEventListener ('click', function (){
-    const clearData = document.getElementById ('typed-pin');
+document.getElementById('clear-btn').addEventListener('click', function () {
+    const clearData = document.getElementById('typed-pin');
     clearData.value = '';
 });
 
 //remove last digit
-document.getElementById ('remove-last-digit').addEventListener ('click', function(){
-    const inputPin = document.getElementById ('typed-pin').value;
-    const removeLastDigit = inputPin.slice (0, -1);
+document.getElementById('remove-last-digit').addEventListener('click', function () {
+    const inputPin = document.getElementById('typed-pin').value;
+    const removeLastDigit = inputPin.slice(0, -1);
     console.log(removeLastDigit);
-    document.getElementById ('typed-pin').value = removeLastDigit;
+    document.getElementById('typed-pin').value = removeLastDigit;
 });
 
- //verify pin
-function verifyPin(){
+//verify pin
+function verifyPin() {
     const pin = document.getElementById('pin').value;
-    const typedPin= document.getElementById ('typed-pin').value;
-    if (pin === '' || typedPin === ''){
+    const typedPin = document.getElementById('typed-pin').value;
+    if (pin === '' || typedPin === '') {
         return;
     }
-    else{
-        if (pin === typedPin){
-            const matched = document.getElementById ('matched');
-            matched.style.display = 'block';
-            document.getElementById ('font-page').style.display = 'none';
-        }
-        else{
-            const notMatched = document.getElementById ('notMatched');
-            // notMatched.style.display = 'block';
-            // document.getElementById ('font-page').style.display = 'none';
+    else if (pin === typedPin) {
+        const matched = document.getElementById('matched');
+        matched.style.display = 'block';
+        document.getElementById('font-page').style.display = 'none';
+    }
+    else {
+        submitLeft = submitLeft - 1;
+        const notMatched = document.getElementById('notMatched');
+        notMatched.style.display = 'block';
+        document.getElementById('font-page').style.display = 'none';
 
-            //By using Alert 
-            alert("Didn't match. Try again");
-            const pin = document.getElementById('pin');
-            pin.value = '';
-            const typedPin= document.getElementById ('typed-pin');
-            typedPin.value = '';
-        }
+        // By using Alert 
+        // alert("Didn't match. Try again");
+        // const pin = document.getElementById('pin');
+        // pin.value = '';
+        // const typedPin= document.getElementById ('typed-pin');
+        // typedPin.value = '';
     }
 };
-document.getElementById ('submitBtn').addEventListener ('click', function (){
+document.getElementById('submitBtn').addEventListener('click', function () {
     verifyPin();
 });
 
 //Retry event
-document.getElementById ('retry-btn').addEventListener ('click', function(){
+document.getElementById('retry-btn').addEventListener('click', function () {
     retry();
 });
 
-function retry (){
-    const secondTry = document.getElementById ('font-page');
-    secondTry.style.display = 'block';
-    secondTry.style.display = 'flex';
-    document.getElementById ('verify').style.display = 'none';
-    const pin = document.getElementById('pin');
-    pin.value = '';
-    const typedPin= document.getElementById ('typed-pin');
-    typedPin.value = '';
+
+function retry() {
+
+    if (submitLeft < 1) {
+        document.getElementById('notMatched').innerText = "❌ No More Chance. You Are Blocked ❌"
+    }
+    else {
+        const secondTry = document.getElementById('font-page');
+        secondTry.style.display = 'flex';
+        document.getElementById('notMatched').style.display = 'none';
+        const pin = document.getElementById('pin');
+        pin.value = '';
+        const typedPin = document.getElementById('typed-pin');
+        typedPin.value = '';
+
+        document.querySelector('.action-left').innerText = `${submitLeft} try left`;
+    }
+
 };
